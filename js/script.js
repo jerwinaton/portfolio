90550ad23065c6d06532b89bba99995a4d183033
Jerwin Mathew Aton <jerwin.mathew@gmail.com>
2022-06-23 21:54:49 +0800

    add skills page

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ $(document).ready(() => {
   //   add transition
   $("#profile-img-bg").find("circle").css("transition", "fill .3s ease");
   $("#profile-img-bg").css("transition", "transform .3s ease");
+
   //   on mouse over change the circle bg color
   //   and scale the image/svg to 1.1
   $("#profile-img-bg").on("mouseover", () => {
     $("#profile-img-bg").find("circle").css("fill", "rgb(49 96 117)");
     $("#profile-img-bg").css("transform", "scale(1.1)");
   });
+
   //   on mouse over change the circle bg color back to default
   // and scale the image/svg back to default
   $("#profile-img-bg").on("mouseleave", () => {
@@ -35,4 +37,29 @@ $(document).ready(() => {
       $("#changing-heading").text(headingTexts[counter]).fadeTo("slow", 1);
     }, 500);
   }, 2700);
+
+  // slick js slider
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
 });
